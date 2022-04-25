623a3bda5e3b46951393aa2cc8f56f0daddf164b
Leonardo Forgiarini Guedes <43677896+leonardofguedes@users.noreply.github.com>
2022-04-25 09:57:55 -0300

    Add files via upload
    1. Melhorando Output de dados ao extrair apenas números(int) e não virgulas.
    2. Aprimorando função ao não mostrar turmas repetidas.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from classes import Person
 from create_db import Cursor
+from itertools import chain
 
 
-def adicionar():
+def adicionar(): # recolhe os dados e insere na Classe
     nome = input('Nome: ->')
     cpf = (input('Cpf: ->'))
     turma = int(input('Turma: ->'))
@@ -10,25 +11,43 @@ def adicionar():
     return student1.__iter__()
 
 
-def filtro(iteravel):
+def filtro(iteravel): # recolhe os dados da Class e insere no Data Base
     lista = []
     for n in iteravel:
         lista.append(n)
     Cursor.execute(f' INSERT INTO Alunos VALUES ("{lista[0]}", "{lista[1]}", "{lista[2]}", "{lista[3]}" ) ')
 
 
-def show():
+def show_all(): # mostra o banco de dados
     Consulta = Cursor.execute(' SELECT * FROM Alunos').fetchall()
     print(Consulta)
 
 
-def app_add_student():
+def list_classes(iteravel): # função para extrair apenas int do iterável e não mostrar turma repetida
+    temp = map(lambda i: str(i), chain.from_iterable(iteravel))
+    d = []
+    for ele in temp:
+        if ele in d:
+            pass
+        else:
+            print(f'Turma: {ele}')
+            d.append(ele)
+
+def show_classes(): # mostra as turmas no DBase
+    Consulta1 = Cursor.execute('SELECT Turma FROM Alunos').fetchall()
+    list_classes(Consulta1)
+
+
+def app_add_student(): # app
     while True:
         filtro(adicionar())
-        dww = input('show?')
+        dww = input('Show all? [Y][N][C]').lower()
         if dww == 'y':
-            show()
-        dwb = input('y')
+            show_all()
+        if dww == 'c':
+            print('Show Classes')
+            show_classes()
+        dwb = input('End program?[Y][N]').lower()
         if dwb == 'y':
             break
 
--- classes.py
+++ classes.py
@@ -3,7 +3,7 @@ class Person:
 
     def __init__(self, nome, cpf, turma):
         Person.id += 1
-        self.__nome = nome.strip().capitalize()
+        self.__nome = nome.strip().title()
         self.__id = Person.id
         self.__cpf = cpf
         self.__turma = turma
