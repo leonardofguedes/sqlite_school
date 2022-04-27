2177a5d209ed684427afb635153172ffb5894e8d
Leonardo Forgiarini Guedes <43677896+leonardofguedes@users.noreply.github.com>
2022-04-27 10:54:59 -0300

    Add files via upload
    Retirei a função de encerrar o programa do def app_add_student e criei uma nova função add_or_end

--- app.py
+++ app.py
@@ -3,27 +3,27 @@ from create_db import Cursor
 from itertools import chain
 
 
-def adicionar(): # recolhe os dados e insere na Classe
+def adicionar():  # recolhe os dados e insere na Classe
     nome = input('Nome: ->')
     cpf = (input('Cpf: ->'))
     turma = int(input('Turma: ->'))
-    student1 = Person(nome,cpf,turma)
+    student1 = Person(nome, cpf, turma)
     return student1.__iter__()
 
 
-def filtro(iteravel): # recolhe os dados da Class e insere no Data Base
+def filtro(iteravel):  # recolhe os dados da Class e insere no Data Base
     lista = []
     for n in iteravel:
         lista.append(n)
     Cursor.execute(f' INSERT INTO Alunos VALUES ("{lista[0]}", "{lista[1]}", "{lista[2]}", "{lista[3]}" ) ')
 
 
-def show_all(): # mostra o banco de dados
+def show_all():  # mostra o banco de dados
     Consulta = Cursor.execute(' SELECT * FROM Alunos').fetchall()
     print(Consulta)
 
 
-def list_classes(iteravel): # função para extrair apenas int do iterável e não mostrar turma repetida
+def list_classes(iteravel):  # função para extrair apenas int do iterável e não mostrar turma repetida
     temp = map(lambda i: str(i), chain.from_iterable(iteravel))
     d = []
     for ele in temp:
@@ -33,31 +33,45 @@ def list_classes(iteravel): # função para extrair apenas int do iterável e n�
             print(f'Turma: {ele}')
             d.append(ele)
 
-def show_classes(): # mostra as turmas no DBase
+
+def show_classes():  # mostra as turmas no DBase
     Consulta1 = Cursor.execute('SELECT Turma FROM Alunos').fetchall()
     list_classes(Consulta1)
 
-def show(): #recebe a info do usuário sobre as turmas na DBase
+
+def show():  # recebe a info do usuário sobre as turmas na DBase
     dww = input('Show all? [Y][N][C]').lower()
     if dww == 'y':
         show_all()
-    if dww == 'c':
+    elif dww == 'c':
         print('Show Classes')
         show_classes()
+    else:
+        pass
 
 
-def app_add_student(): # app
+def add_or_end(): # função para encerrar o programa ou continuar a adicionar
+    while True:
+        try:
+            a = input('Do you want to ADD more or END the program? [ADD][END]').strip().lower()
+            if a == 'end':
+                return False
+            elif a == 'add':
+                return True
+            else:
+                print('Wrong answer')
+        except:
+            print('Wrong answer')
+
+
+def app_add_student():  # app
     while True:
         filtro(adicionar())
         show()
-        try:
-            dwb = input('End program?[Y][N]').lower()
-            if dwb == 'y':
-                break
-            elif dwb == 'n':
-                pass
-        except:
-            print('Not Understand')
+        if not add_or_end():
+            break
+        else:
+            pass
 
 
 app_add_student()
